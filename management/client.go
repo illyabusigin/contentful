d55221d968fd5ec4c92ca74718234add23690481
Illya Busigin <illya.busigin@ingaged.co>
2016-07-19 10:14:48 -0500

    Fleshing out Spaces CRUD

---
+++ management/client.go
@@ -0,0 +1,71 @@
+package management
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	rate "github.com/beefsack/go-rate"
+	"github.com/ingaged/sling"
+)
+
+const baseURL = "https://api.contentful.com"
+
+type Client struct {
+	AccessToken string
+
+	sling *sling.Sling
+	rl    *rate.RateLimiter
+}
+
+////////////////////
+// Initialization //
+////////////////////
+
+// NewClient creates a new Contentful API client
+func NewClient(accessToken string, version string, httpClient *http.Client) *Client {
+	client := &Client{
+		AccessToken: accessToken,
+		sling: sling.New().Client(httpClient).Base(baseURL).
+			Set("Content-Type", contentTypeHeader(version)).
+			Set("Authorization", authorizationHeader(accessToken)),
+	}
+
+	client.rl = rate.New(10, time.Second*1)
+
+	return client
+}
+
+func contentTypeHeader(version string) string {
+	return fmt.Sprintf("application/vnd.contentful.management.%v+json", version)
+}
+
+func authorizationHeader(accessToken string) string {
+	return fmt.Sprintf("Bearer %v", accessToken)
+}
+
+////////////////
+// Base Types //
+////////////////
+
+type System struct {
+	ID        string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+	Type    string
+	Version int
+}
+
+type ContentfulError struct {
+	RequestID string `json:"requestId"`
+	Message   string `json:"message"`
+	Sys       struct {
+		Type string `json:"type"`
+		ID   string `json:"id"`
+	} `json:"sys"`
+}
+
+func (e *ContentfulError) Error() string {
+	return e.Message
+}
---
+++ management/spaces.go
@@ -0,0 +1,186 @@
+package management
+
+import (
+	"fmt"
+	"time"
+)
+
+// Space is a container for content types, entries and assets and other resources.
+// API consumers, like mobile apps or websites, typically fetch data by getting
+// entries and assets from one or more spaces.
+type Space struct {
+	System `json:"-"`
+	Name   string `json:"name"`
+}
+
+type contentfulSpace struct {
+	Name string `json:"name"`
+	Sys  struct {
+		Type      string `json:"type"`
+		ID        string `json:"id"`
+		Version   int    `json:"version"`
+		CreatedBy struct {
+			Sys struct {
+				Type     string `json:"type"`
+				LinkType string `json:"linkType"`
+				ID       string `json:"id"`
+			} `json:"sys"`
+		} `json:"createdBy"`
+		CreatedAt time.Time `json:"createdAt"`
+		UpdatedBy struct {
+			Sys struct {
+				Type     string `json:"type"`
+				LinkType string `json:"linkType"`
+				ID       string `json:"id"`
+			} `json:"sys"`
+		} `json:"updatedBy"`
+		UpdatedAt time.Time `json:"updatedAt"`
+	} `json:"sys"`
+}
+
+func (c *contentfulSpace) Convert() *Space {
+	space := new(Space)
+
+	space.ID = c.Sys.ID
+	space.CreatedAt = c.Sys.CreatedAt
+	space.UpdatedAt = c.Sys.UpdatedAt
+
+	space.Type = c.Sys.Type
+	space.Version = c.Sys.Version
+
+	space.Name = c.Name
+
+	return space
+}
+
+type AllSpacesResponse struct {
+	Spaces []Space
+	Error  error
+}
+
+// GetAllSpaces returns all spaces associated with the account
+func (c *Client) GetAllSpaces() (response AllSpacesResponse) {
+	c.rl.Wait()
+
+	type SpacesResponse struct {
+		Total int `json:"total"`
+		Limit int `json:"limit"`
+		Skip  int `json:"skip"`
+		Sys   struct {
+			Type string `json:"type"`
+		} `json:"sys"`
+		Items []contentfulSpace `json:"items"`
+	}
+
+	spacesData := new(SpacesResponse)
+	contentfulError := new(ContentfulError)
+	path := fmt.Sprintf("spaces")
+	_, err := c.sling.New().Get(path).Receive(spacesData, contentfulError)
+
+	if contentfulError.Message != "" {
+		err = contentfulError
+		return
+	}
+
+	if err != nil {
+		response.Error = err
+	} else {
+		response.Spaces = make([]Space, 0)
+
+		for _, space := range spacesData.Items {
+			response.Spaces = append(response.Spaces, space.Convert())
+		}
+	}
+
+	return
+}
+
+// CreateSpace will create a space with the provided name. It's important to
+// note that names are not unique between spaces.
+func (c *Client) CreateSpace(name string) (space *Space, err error) {
+	c.rl.Wait()
+
+	spaceData := new(contentfulSpace)
+	contentfulError := new(ContentfulError)
+	path := fmt.Sprintf("spaces")
+	_, err = c.sling.New().Post(path).Receive(spaceData, contentfulError)
+
+	if contentfulError.Message != "" {
+		err = contentfulError
+		return
+	}
+
+	if err == nil {
+		space = spaceData.Convert()
+	}
+
+	return
+}
+
+// FetchSpace will return a space for the given identifier.
+func (c *Client) FetchSpace(identifier string) (space *Space, err error) {
+	c.rl.Wait()
+
+	spaceData := new(contentfulSpace)
+	contentfulError := new(ContentfulError)
+	path := fmt.Sprintf("spaces/%v", identifier)
+	_, err = c.sling.New().Get(path).Receive(spaceData, contentfulError)
+
+	if contentfulError.Message != "" {
+		err = contentfulError
+		return
+	}
+
+	if err == nil {
+		space = spaceData.Convert()
+	}
+
+	return
+}
+
+// UpdateSpace will update the space
+func (c *Client) UpdateSpace(space *Space) (updated *Space, err error) {
+	if space == nil {
+		return nil, fmt.Errorf("Unable to update. Space argument was nil!")
+	}
+
+	c.rl.Wait()
+
+	spaceData := new(contentfulSpace)
+	contentfulError := new(ContentfulError)
+	path := fmt.Sprintf("spaces/%v", space.System.ID)
+	_, err = c.sling.New().
+		Set("X-Contentful-Version", fmt.Sprintf("%v", space.System.Version)).
+		Put(path).
+		BodyJSON(space).
+		Receive(spaceData, contentfulError)
+
+	if contentfulError.Message != "" {
+		err = contentfulError
+		return
+	}
+
+	if err == nil {
+		updated = spaceData.Convert()
+	}
+
+	return
+}
+
+// DeleteSpace will delete an existing space by doing a DELETE request to /spaces/ID.
+// Note that deleting a space will remove its entire content, including all content
+// types, entries and assets. Be careful as this action can not be undone.
+func (c *Client) DeleteSpace(identifier string) (err error) {
+	c.rl.Wait()
+
+	contentfulError := new(ContentfulError)
+	path := fmt.Sprintf("spaces/%v", identifier)
+	_, err = c.sling.New().Delete(path).Receive(nil, contentfulError)
+
+	if contentfulError.Message != "" {
+		err = contentfulError
+		return
+	}
+
+	return
+}
