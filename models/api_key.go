1c2ec77d7fa97c4ab5c83d972770ac0bcfac352a
Illya Busigin <illya.busigin@ingaged.co>
2016-10-28 19:44:19 -0500

    Adding AccessToken parameter to API key

--- models/api_key.go
+++ models/api_key.go
@@ -3,5 +3,7 @@ package models
 // APIKey represents an API Key object. The actual key value is located in the sys.id field.
 type APIKey struct {
 	System `json:"sys"`
-	Name   string
+
+	Name        string `json:"name"`
+	AccessToken string `json:"accesstoken"`
 }
